cb7a1dd38ccab0c7917da7c08f77b74b41c0c411
lEnrikeC <luiscortes02@uv.mx>
2026-03-12 14:44:23 -0600

    Feature/deshabilitar pruebas (#11)
    * descomentar codigo
    
    * comentar pruebas

--- tests/TaskApi.Tests/TaskControllerTests.cs
+++ tests/TaskApi.Tests/TaskControllerTests.cs
@@ -15,50 +15,50 @@ public class TaskControllerTests{
         _ctrl = new TasksController(_mockRepo.Object);
     }
 
-    //probar GetAll cuando hay tarea retorna Ok con lista de tareas
-    [Fact]
-    public void GetAll_RepositorioConTareas_RetornaOkConListaDeTareas(){
-        //Arrange
-        var tareas = new List<TaskItem> {
-            new TaskItem { Id = 1, Title = "Tarea 1", Description = "Descripción de la tarea 1", IsCompleted = false },
-            new TaskItem { Id = 2, Title = "Tarea 2", Description = "Descripción de la tarea 2", IsCompleted = true }
-        };
-        _mockRepo.Setup(r => r.GetAll()).Returns(tareas);
-        //Act
-        var resultado = _ctrl.GetAll();
-        //Assert
-        var okResult = resultado.Should().BeOfType<OkObjectResult>().Subject;
-        var returnedTareas = okResult.Value.Should().BeAssignableTo<IEnumerable<TaskItem>>().Subject;
-        returnedTareas.Should().HaveCount(2);
-    }
+    // //probar GetAll cuando hay tarea retorna Ok con lista de tareas
+    // [Fact]
+    // public void GetAll_RepositorioConTareas_RetornaOkConListaDeTareas(){
+    //     //Arrange
+    //     var tareas = new List<TaskItem> {
+    //         new TaskItem { Id = 1, Title = "Tarea 1", Description = "Descripción de la tarea 1", IsCompleted = false },
+    //         new TaskItem { Id = 2, Title = "Tarea 2", Description = "Descripción de la tarea 2", IsCompleted = true }
+    //     };
+    //     _mockRepo.Setup(r => r.GetAll()).Returns(tareas);
+    //     //Act
+    //     var resultado = _ctrl.GetAll();
+    //     //Assert
+    //     var okResult = resultado.Should().BeOfType<OkObjectResult>().Subject;
+    //     var returnedTareas = okResult.Value.Should().BeAssignableTo<IEnumerable<TaskItem>>().Subject;
+    //     returnedTareas.Should().HaveCount(2);
+    // }
 
-    //Get by id cuando la tarea existe retorna Ok con la tarea
-    [Fact]
-    public void GetById_TareaExistente_RetornaOkConTarea(){
-        //Arrange
-        var tarea = new TaskItem { Id = 1, Title = "Tarea 1", Description = "Descripción de la tarea 1", IsCompleted = false };
-        _mockRepo.Setup(r => r.GetById(1)).Returns(tarea);
-        //Act
-        var resultado = _ctrl.GetById(1);
-        //Assert
-        var okResult = resultado.Should().BeOfType<OkObjectResult>().Subject;
-        var returnedTarea = okResult.Value.Should().BeAssignableTo<TaskItem>().Subject;
-        returnedTarea.Should().Be(tarea);
-    }
+    // //Get by id cuando la tarea existe retorna Ok con la tarea
+    // [Fact]
+    // public void GetById_TareaExistente_RetornaOkConTarea(){
+    //     //Arrange
+    //     var tarea = new TaskItem { Id = 1, Title = "Tarea 1", Description = "Descripción de la tarea 1", IsCompleted = false };
+    //     _mockRepo.Setup(r => r.GetById(1)).Returns(tarea);
+    //     //Act
+    //     var resultado = _ctrl.GetById(1);
+    //     //Assert
+    //     var okResult = resultado.Should().BeOfType<OkObjectResult>().Subject;
+    //     var returnedTarea = okResult.Value.Should().BeAssignableTo<TaskItem>().Subject;
+    //     returnedTarea.Should().Be(tarea);
+    // }
 
-    //Get by id cuando la tarea no existe retorna NotFound result
-    [Fact]    
-    public void GetById_TareaNoExistente_RetornaNotFound(){
-        //Arrange
-        _mockRepo.Setup(r => r.GetById(1)).Returns((TaskItem?)null);
-        //Act
-        var resultado = _ctrl.GetById(1);
-        //Assert
-        resultado.Should().BeOfType<NotFoundResult>();
+    // //Get by id cuando la tarea no existe retorna NotFound result
+    // [Fact]    
+    // public void GetById_TareaNoExistente_RetornaNotFound(){
+    //     //Arrange
+    //     _mockRepo.Setup(r => r.GetById(1)).Returns((TaskItem?)null);
+    //     //Act
+    //     var resultado = _ctrl.GetById(1);
+    //     //Assert
+    //     resultado.Should().BeOfType<NotFoundResult>();
 
-    }
+    // }
 
-    
+
 
 
 }
